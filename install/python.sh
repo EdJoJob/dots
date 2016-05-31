84eedd10b1f4849a02fdb0f4d9922eb91fe935b9
Edward Evans <edjojob@gmail.com>
2016-05-31 11:15:05 +1000

    python: include py.test and ptw functions

--- install/python.sh
+++ install/python.sh
@@ -1,4 +1,4 @@
 pip2 install --upgrade pip setuptools virtualenv
-pip2 install q docutils pygments keyring mercurial mercurial-keyring neovim bpython jedi pycd
+pip2 install q docutils pygments keyring mercurial mercurial-keyring neovim bpython jedi pycd pytest pytest-watch
 pip3 install --upgrade pip setuptools virtualenv
-pip3 install neovim 
+pip3 install neovim pytest pytest-watch
