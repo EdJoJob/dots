752c118865338a475c76aef565b03cdb9982fc2c
Edward Evans <edjojob@gmail.com>
2017-01-03 08:39:53 +1000

    Add new useful python modules

--- install/python.sh
+++ install/python.sh
@@ -1,4 +1,6 @@
 pip2 install --upgrade pip setuptools virtualenv
-pip2 install q docutils pygments keyring mercurial mercurial-keyring neovim jedi pycd ptpython
+pip2 install docutils pygments keyring mercurial mercurial-keyring neovim pycd ptpython
 pip3 install --upgrade pip setuptools virtualenv
-pip3 install neovim ptpython
+pip3 install neovim ptpython jedi q
+pip3 install flake8 flake8-builtins flake8-tidy-imports flake8-todo
+pip3 install isort
