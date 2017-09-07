d8e10044b0631433a97f2d156eab9da05ffe2331
Edward Evans <edjojob@gmail.com>
2017-09-08 08:33:12 +1000

    Firefox add new vimfx command mappings

--- firefox/vimfx.symlink/config.js
+++ firefox/vimfx.symlink/config.js
@@ -1,10 +1,9 @@
-
 let {commands} = vimfx.modes.normal
+
 vimfx.addCommand({
   name: 'search_tabs',
   description: 'Search tabs',
   category: 'tabs',
-  order: commands.focus_location_bar.order + 1,
 }, (args) => {
   let {vim} = args
   let {gURLBar} = vim.window
@@ -14,10 +13,12 @@ vimfx.addCommand({
   gURLBar.value = '% '
   gURLBar.onInput(new vim.window.KeyboardEvent('input'))
 });
+vimfx.set('custom.mode.normal.search_tabs', 'b')
 
 vimfx.addCommand({
     name: "closetabtree",
     description: "Close the current tab tree",
+    category: 'tabs',
 }, ({vim}) => {
     let childTab = null;
     let browser = vim.window.gBrowser;
@@ -30,11 +31,13 @@ vimfx.addCommand({
     }
     browser.removeTab( browser.mCurrentTab );
 });
+vimfx.set('custom.mode.normal.closetabtree', '<c-x>')
 
 
 vimfx.addCommand({
     name: "closefulltabtree",
     description: "Close the full tab tree from the root",
+    category: 'tabs',
 }, ({vim}) => {
     let browser = vim.window.gBrowser;
     var rootTab = vim.window.TreeStyleTabService.getRootTab(browser.mCurrentTab);
@@ -53,4 +56,5 @@ vimfx.addCommand({
     }
     browser.removeTab( rootTab );
 });
+vimfx.set('custom.mode.normal.closefulltabtree', '<c-X>')
 
