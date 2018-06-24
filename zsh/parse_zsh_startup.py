68e7caaaa9820d22f5dda5ad9742b9f72724101b
EdJoJob <edjojob@gmail.com>
2018-06-25 09:15:27 +1000

    Make script python2/3 compatible

--- zsh/parse_zsh_startup.py
+++ zsh/parse_zsh_startup.py
@@ -1,18 +1,20 @@
-import datetime
+from __future__ import print_function
 import sys
 
 SLOW_THRESHOLD = 7
+
 
 def parse_line(raw_line):
     nonewline = raw_line.strip('\n')
     timestr, rest = nonewline.split(' ', 1)
     return int(timestr), rest
 
+
 def main(filename):
     with open(filename) as f:
         count = 0
         start_time, rest = parse_line(f.readline())
-        print "0 {line}".format(line=rest)
+        print("0 {line}".format(line=rest))
 
         prev_line = rest
         prev_line_start = start_time
@@ -27,13 +29,16 @@ def main(filename):
                 t, rest = parse_line(line)
                 diff = t - prev_line_start
                 if diff > SLOW_THRESHOLD:
-                    print "{since_start} {diff} {prev_line}".format(
-                        since_start=t-start_time, diff=diff, prev_line=prev_line)
+                    print("{since_start} {diff} {prev_line}".format(
+                        since_start=t - start_time,
+                        diff=diff,
+                        prev_line=prev_line,
+                    ))
                 prev_line_start = t
                 prev_line = rest
             except ValueError:
                 continue
 
+
 if __name__ == "__main__":
     main(sys.argv[1])
-
