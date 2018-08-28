81f637d9886854e728fda8333ccfbf259c014c9a
EdJoJob <edjojob@gmail.com>
2018-08-28 13:44:34 +1000

    Include pre-commit. It is just too useful

--- install/python.sh
+++ install/python.sh
@@ -4,3 +4,4 @@ pip3 install --user --upgrade pip setuptools virtualenv
 pip3 install --user neovim ptpython jedi q
 pip3 install --user flake8 flake8-builtins flake8-tidy-imports flake8-todo
 pip3 install --user isort
+pip3 install --user pre-commit
