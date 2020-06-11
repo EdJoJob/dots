946a7041dbd8047203f5bd9c6c380136f3358f39
EdJoJob <edjojob@gmail.com>
2020-06-11 13:38:54 +1000

    Allow movement between numbers and screens to match left/right movements
    Signed-off-by: EdJoJob <edjojob@gmail.com>

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,3 +1,4 @@
+import Control.Monad (liftM2)
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -103,13 +104,13 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-shift-[1..9] %! Move client to workspace N
     [((m .|. controlMask .|. mod4Mask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.greedyView, 0), (liftM2 (.) W.greedyView W.shift, mod1Mask)]]
     ++
     -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
     [((m .|. controlMask .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.view, 0), (W.shift, mod1Mask)]]
 
 myLayout = onWorkspace "5:gimp" gimp $
            onWorkspace "3:term"  tiled $
